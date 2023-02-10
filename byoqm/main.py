850c5ebdb9bed2dfe607854cfb9a10105e6411a1
SadCheeto <caspar.marschall@me.com>
2023-02-10 12:42:43 +0100

    changed black command in readme and ran black

--- byoqm/main.py
+++ byoqm/main.py
@@ -3,11 +3,11 @@ import os
 from metric import Metric
 
 
-path = './metrics'
+path = "./metrics"
 metrics = []
 for child in Path(path).iterdir():
     if child.is_file():
         metrics.append(Metric(child.resolve()))
-        
+
 for metric in metrics:
-    print(metric.name, ':' ,metric.measure())
+    print(metric.name, ":", metric.measure())
--- byoqm/metric/metric.py
+++ byoqm/metric/metric.py
@@ -3,15 +3,12 @@ import subprocess
 from pathlib import Path
 
 
-
-class Metric():
-    def __init__(self, path : Path):
+class Metric:
+    def __init__(self, path: Path):
         self.path = path.resolve()
         self.name = path.name
-        
+
     def measure(self) -> int | float:
         process = subprocess.run([self.path], stdout=subprocess.PIPE)
-        result = process.stdout.decode('utf-8').strip()
+        result = process.stdout.decode("utf-8").strip()
         return result
-        
-        
