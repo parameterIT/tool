4a0b63ecc54a115934071aaafb984a23c508c593
Jakob <73228675+Kobo-coder@users.noreply.github.com>
2023-02-10 15:53:19 +0100

    First iteration of QM interface implementation

--- byoqm/qualitymodel/treequalitymodel.py
+++ byoqm/qualitymodel/treequalitymodel.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from ast import List
 from typing import Callable
+from byoqm.metric.metric import Metric
 from qualitymodel import Characteristic, QualityModel
 
 class Node(Characteristic):
@@ -14,20 +15,36 @@ class Node(Characteristic):
         measurement = None
         for c in self.children:
             measurement += c.measure()
+        return measurement
 
 
 class TreeQualityModel(QualityModel):
     def __init__(self):
         self.root = Node("root", None, [])
 
-    def insert(self, children, parent, weights: List[float | int]):
-        self.find(parent).append(children)
+    def insert(self, parent, child):
+        self.find(parent).children.append(child)
 
-    def keys():
-        pass
+    def keys(self):
+        print(self._keys(self.root,""))
+        
+    def _keys(self, node: Node, metrics: str):
+        for n in node.children:
+            if isinstance(n, Metric):
+                metrics += n.name
+            else:
+                self._keys(n, metrics)
 
-    def find():
-        pass
+    def find(self, name: str):         
+        for n in self.root.children:
+            n._find(n, name)
+
+    def _find(self, node: Node, name: str):
+        if node.name == name:
+            return node
+        else:
+            for n in node.children:
+                n.find(n, name)
     
-    def measure():
-        pass
+    def measure(self):
+        self.root.measure()
