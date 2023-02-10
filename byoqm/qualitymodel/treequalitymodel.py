8d82b81dcf19c2cb3aa85b4763adeaccf358a1ff
Jakob <73228675+Kobo-coder@users.noreply.github.com>
2023-02-10 15:22:45 +0100

    Add WIP init for QM interface implementation

---
+++ byoqm/qualitymodel/treequalitymodel.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+from ast import List
+from typing import Callable
+from qualitymodel import Characteristic, QualityModel
+
+class Node(Characteristic):
+    def __init__(self, name: str, parent: Node, weights: List[int | float]):
+        self.name = name
+        self.parent = parent
+        self.children = []
+        self.weights = weights
+
+    def measure(self):
+        measurement = None
+        for c in self.children:
+            measurement += c.measure()
+
+
+class TreeQualityModel(QualityModel):
+    def __init__(self):
+        self.root = Node("root", None, [])
+
+    def insert(self, children, parent, weights: List[float | int]):
+        self.find(parent).append(children)
+
+    def keys():
+        pass
+
+    def find():
+        pass
+    
+    def measure():
+        pass
