8591450501477ed11c07c927fddb1e5897e0b404
Caspar Marschall <60236750+SadCheeto@users.noreply.github.com>
2023-02-10 13:19:57 +0100

    Dummymetrics (#6)
    * dummy metrics implemented
    
    * changed black command in readme and ran black

--- README.md
+++ README.md
@@ -26,6 +26,6 @@ poetry run python -m unittest
 To format:
 
 ```
-poetry run python black .
+poetry run python -m black .
 ```
 
--- byoqm/main.py
+++ byoqm/main.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+from pathlib import Path
+import os
+from metric import Metric
+
+
+path = "./metrics"
+metrics = []
+for child in Path(path).iterdir():
+    if child.is_file():
+        metrics.append(Metric(child.resolve()))
+
+for metric in metrics:
+    print(metric.name, ":", metric.measure())
