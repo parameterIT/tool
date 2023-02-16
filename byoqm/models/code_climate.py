8b6c3b05ca63c6bacad57089233ec07b17659cfb
Caspar Marschall <60236750+SadCheeto@users.noreply.github.com>
2023-02-16 14:38:04 +0100

    Argument count (#17)
    * method argument count implemented
    
    * black
    
    * method call on close

--- byoqm/models/code_climate.py
+++ byoqm/models/code_climate.py
@@ -26,7 +26,18 @@ class CodeClimate(QualityModel):
         pass
 
     def argument_count(self):
-        pass
+        py_files = self.src_root.glob("**/*.py")
+        count = 0
+        for file in py_files:
+            with open(file) as f:
+                tree = ast.parse(f.read())
+                for exp in tree.body:
+                    if not isinstance(exp, ast.FunctionDef):
+                        continue
+                    if len(exp.args.args) > 4:
+                        count += 1
+        py_files.close()
+        return count
 
     def complex_logic(self):
         pass
@@ -57,7 +68,7 @@ class CodeClimate(QualityModel):
                 mc = sum(isinstance(exp, ast.FunctionDef) for exp in tree.body)
                 if mc > 20:
                     count += 1
-        py_files.close
+        py_files.close()
         return count
 
     def method_length(self):
--- byoqm/models/test/test_code_climate.py
+++ byoqm/models/test/test_code_climate.py
@@ -23,6 +23,15 @@ class TestCodeClimate(unittest.TestCase):
 
         self.assertEqual(actual, expected)
 
+    def test_argument_count_given_this_directory_returns_2(self):
+        qm = CodeClimate()
+        qm.set_src_root(Path("byoqm/models/test"))
+
+        expected = 2
+        actual = qm.argument_count()
+
+        self.assertEqual(actual, expected)
+
 
 if __name__ == "__main__":
     unittest.main()
