b3ca40171545a894b5e954a4ee97d1409595e5f4
SadCheeto <caspar.marschall@me.com>
2023-02-17 13:20:08 +0100

    indent change

--- byoqm/models/code_climate.py
+++ byoqm/models/code_climate.py
@@ -90,8 +90,7 @@ class CodeClimate(QualityModel):
                 for child in children:
                     if child.type != "function_definition":
                         continue
-                    query = self._py_language.query(
-                        """
+                    query = self._py_language.query("""
                     (function_definition
                         name: (identifier) @function.def)
                     (call 
