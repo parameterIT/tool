c5a3ae3dcec5fc8802935de4a693a3dd300e9cfb
Jakob <73228675+Kobo-coder@users.noreply.github.com>
2023-02-22 14:58:21 +0100

    add out to output of duplicate code

--- byoqm/models/code_climate.py
+++ byoqm/models/code_climate.py
@@ -69,7 +69,7 @@ class CodeClimate(QualityModel):
         for file in self.src_root.glob("**/*.py"):
             files.append(str(file))
         filestring = f"{files}"; filestring = filestring[1:len(filestring)-1]
-        os.system(f"metrics/cpd/bin/run.sh cpd --minimum-tokens 10 --skip-lexical-errors --dir {filestring} --format xml > result.xml")
+        os.system(f"metrics/cpd/bin/run.sh cpd --minimum-tokens 10 --skip-lexical-errors --dir {filestring} --format xml > out/result.xml")
         et = parse("result.xml")
         count = 0
         for child in et.getroot():
