b18b53d0079b34269da07b37e97429b4d51adc42
SadCheeto <caspar.marschall@me.com>
2023-02-23 16:02:13 +0100

    hehehe black

--- byoqm/visuals/dashboard.py
+++ byoqm/visuals/dashboard.py
@@ -5,30 +5,29 @@ from visuals import line
 import csv
 import os
 
-class Dashboard:
 
+class Dashboard:
     def show_graphs(self):
         data = self.get_data()
         f = []
-        for key in data: 
+        for key in data:
             f.append(line.get_line(data, key))
-        grid = gridplot([[f[0], f[1]], [f[2], f[3]], [f[4], f[5]]])    
+        grid = gridplot([[f[0], f[1]], [f[2], f[3]], [f[4], f[5]]])
         show(grid)
-            
+
     def sort_data_values(self, data):
         for key in data:
             data[key].sort()
         return data
-                    
-        
-    def get_data(self, path = "./output"):    
+
+    def get_data(self, path="./output"):
         graph_data = {}
 
         for filename in os.listdir(path):
-            date = filename.split('.')[0]
+            date = filename.split(".")[0]
             file_path = os.path.join(path, filename)
-            if os.path.isfile(file_path):   
-                with open(file_path, 'r') as csvfile:
+            if os.path.isfile(file_path):
+                with open(file_path, "r") as csvfile:
                     datareader = csv.reader(csvfile)
                     for row in datareader:
                         if row[0] == "Metric":
@@ -36,10 +35,7 @@ class Dashboard:
                         tuple = (date, int(row[1]))
                         if not row[0] in graph_data:
                             graph_data[row[0]] = [tuple]
-                        else: 
+                        else:
                             graph_data[row[0]].append(tuple)
-            
+
         return self.sort_data_values(graph_data)
-    
-    
-    
