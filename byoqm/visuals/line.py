a3ecdf9519647218fb2f116ad2e66fd4f79e4531
FrederikRothe <frederi.rothe@gmail.com>
2023-02-24 09:10:54 +0100

    name fix

--- byoqm/visuals/line.py
+++ byoqm/visuals/line.py
@@ -2,10 +2,10 @@ from bokeh.plotting import figure
 
 
 def get_line(data, key):
-    newX = [(value[0].split("-")[2]) for value in data[key]]
-    newY = [(value[1]) for value in data[key]]
+    x = [(value[0].split("-")[2]) for value in data[key]]
+    y = [(value[1]) for value in data[key]]
     p = figure(
         title=key, x_axis_label="Date", y_axis_label="Problems", width=600, height=350
     )
-    p.line(newX, newY, legend_label="Progress", line_width=2)
+    p.line(x, y, legend_label="Progress", line_width=2)
     return p
