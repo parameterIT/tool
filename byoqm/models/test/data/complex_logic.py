ae793dbe86885fd9117dbb1ee6e6e51b9f28e12f
Caspar Marschall <60236750+SadCheeto@users.noreply.github.com>
2023-02-24 12:32:36 +0100

    Complex logic (#27)
    * still in progress
    
    * black
    
    * black
    
    * indent change
    
    * black is having a laugh
    
    * added folder, and package to poetry
    
    * added pie chart, dummy data
    
    * not done
    
    * fixed poetry
    
    * Implemented a basic dashboard
    
    * hehehe black
    
    * gitignore
    
    * Mehtod length doneso
    
    * removed children
    
    * black
    
    * poetry update
    
    * complex logic works
    
    * added check for longer boolean chains
    
    * test_code
    
    * black
    
    * no pie
    
    * rm main
    
    ---------
    
    Co-authored-by: FrederikRothe <frederi.rothe@gmail.com>

---
+++ byoqm/models/test/data/complex_logic.py
@@ -0,0 +1,24 @@
+a = bool(1)
+b = bool(0)
+c = bool(0)
+d = bool(1)
+
+if a or b or c:
+    print(2)
+elif b or c or d or a:
+    print(3)
+
+while b or c:
+    print(3)
+
+
+def method1():
+    print(1)
+    if a or b:
+        print(1)
+
+    if a and b or c:
+        print(2)
+
+
+method1()
