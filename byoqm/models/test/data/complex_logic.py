96be65687686b7d3ecc4a9feac323081b79e92b8
SadCheeto <caspar.marschall@me.com>
2023-02-26 12:11:59 +0100

    complex logic now also allows for call statements

--- byoqm/models/test/data/complex_logic.py
+++ byoqm/models/test/data/complex_logic.py
@@ -3,6 +3,14 @@ b = bool(0)
 c = bool(0)
 d = bool(1)
 
+def returns_true():
+    return bool(1)
+
+def return_false():
+    return bool(0)
+if return_false() or returns_true():
+    print(1)
+    
 if a or b or c:
     print(2)
 elif b or c or d or a:
