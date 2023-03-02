0d509eca19d81636c6d16894e5670a9d2e4b0d61
SadCheeto <caspar.marschall@me.com>
2023-03-02 14:52:23 +0100

    printing quality model ruins the dashboard

--- byoqm/runner.py
+++ byoqm/runner.py
@@ -94,8 +94,8 @@ class Runner:
 
         with open(file_location, "w") as results_file:
             writer = csv.writer(results_file)
-            writer.writerow([f"qualitymodel={self._model_name}"])
-            writer.writerow(["Metric", "Value"])
+            # writer.writerow([f"qualitymodel={self._model_name}"])
+            writer.writerow(["metric", "value"])
             for description, value in results.items():
                 writer.writerow([description, value])
 
