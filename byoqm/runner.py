41d0f42ca5f11425da531f57784a7163703ae355
SadCheeto <caspar.marschall@me.com>
2023-03-02 15:08:21 +0100

    fixed previously mentioned problem with qm

--- byoqm/runner.py
+++ byoqm/runner.py
@@ -94,7 +94,7 @@ class Runner:
 
         with open(file_location, "w") as results_file:
             writer = csv.writer(results_file)
-            # writer.writerow([f"qualitymodel={self._model_name}"])
+            writer.writerow([f"qualitymodel={self._model_name}"])
             writer.writerow(["metric", "value"])
             for description, value in results.items():
                 writer.writerow([description, value])
