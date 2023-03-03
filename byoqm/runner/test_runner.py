b5d34f5f5b29b492a794fa89620e400776dc65cd
Christoffer Gram <chkg@itu.dk>
2023-03-03 09:47:50 +0100

    Test _load in Runner
    Hard to test the other stuff, because there is currently no way to
    redirect where files are output.

---
+++ byoqm/runner/test_runner.py
@@ -0,0 +1,26 @@
+import unittest
+
+from pathlib import Path
+from byoqm.runner import Runner
+
+
+class TestRunner(unittest.TestCase):
+    def setUp(self):
+        self._runner = Runner("code_climate", Path("."))
+
+    def test_load_given_code_climate_inits_runner(self):
+        self._runner._load("code_climate")
+        # Not crashing on the above line, means that code_climate was successfully located
+        # and found, so assert true
+        self.assertTrue(True)
+
+    def test_load_given_the_non_existent_model_raises_FileNotFoundError(self):
+        self.assertRaises(
+            FileNotFoundError,
+            self._runner._load,
+            "this model has never, does not, and will never exist (hopefully)",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
