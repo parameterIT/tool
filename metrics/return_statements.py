6322c544af743d5c47877b9aeac8d34427003fc6
SadCheeto <caspar.marschall@me.com>
2023-03-03 11:22:59 +0100

    wip

--- metrics/return_statements.py
+++ metrics/return_statements.py
@@ -4,6 +4,10 @@ import ast
 from tree_sitter import Language, Parser, Node
 import sys
 
+PY_LANGUAGE = Language("./build/my-languages.so", "python")
+parser = Parser()
+parser.set_language(PY_LANGUAGE)
+
 
 def parse_src_root() -> Path:
     if len(sys.argv) == 1:
@@ -18,22 +22,29 @@ def parse_src_root() -> Path:
     return path_to_src
 
 
-PY_LANGUAGE = Language("./build/my-languages.so", "python")
-parser = Parser()
-parser.set_language(PY_LANGUAGE)
-src = parse_src_root()
+def parse():
+    count = 0
+    src = parse_src_root()
+    if src.is_file():
+        with src.open() as f:
+            count = _parse(f)
+    else:
+        py_files = src.glob("**/*.py")
+        for file in py_files:
+            with open(file) as f:
+                count += _parse(f)
+        py_files.close()
+    return count
 
 
-py_files = src.glob("**/*.py")
-count = 0
-for file in py_files:
-    with open(file) as f:
-        tree = ast.parse(f.read())
-        for node in tree.body:
-            if isinstance(node, ast.FunctionDef):
-                rs = sum(isinstance(subexp, ast.Return) for subexp in ast.walk(node))
-                if rs > 4:
-                    count += 1
-py_files.close()
+def _parse(file):
+    count = 0
+    tree = ast.parse(file.read())
+    for node in tree.body:
+        if isinstance(node, ast.FunctionDef):
+            rs = sum(isinstance(subexp, ast.Return) for subexp in ast.walk(node))
+            if rs > 4:
+                count += 1
 
-print(count)
+
+print(parse())
