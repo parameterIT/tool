120dc428917e75edf17521b73e6eed90a046bc04
Christoffer Gram <chkg@itu.dk>
2023-03-03 11:28:31 +0100

    Runner tests (#40)
    * Move runner.py into runner
    
    * Fix Impor
    
    * Test _load in Runner
    
    Hard to test the other stuff, because there is currently no way to
    redirect where files are output.
    
    * Add run test + setUp and tearDown of output folder for tests
    
    * Add no_cpd.py model so tests don't need to use CPD
    
    * Delete Metrics from no_cpd
    
    * Clean-up Imports in models

---
+++ byoqm/runner/test_runner.py
@@ -0,0 +1,47 @@
+import unittest
+
+from pathlib import Path
+
+from test.test_support import os, shutil
+
+from byoqm.runner import Runner
+
+_TEST_FOLDER = Path("byoqm/runner/test")
+_OUTPUT_FOLDER = _TEST_FOLDER / Path("test")
+
+
+class TestRunner(unittest.TestCase):
+    def setUp(self):
+        _OUTPUT_FOLDER.mkdir(parents=True, exist_ok=True)
+
+        self._runner = Runner(
+            "no_cpd",
+            Path("byoqm/"),
+            _OUTPUT_FOLDER,
+            True,
+        )
+
+    def tearDown(self):
+        # shutil over os.rmdir, because it allows you to remove non/empty directories
+        shutil.rmtree(_TEST_FOLDER, ignore_errors=True)
+
+    def test_load_given_code_climate_inits_runner(self):
+        self._runner._load("code_climate")
+        # Not crashing on the above line, means that code_climate was successfully located
+        # and found, so assert true
+        self.assertTrue(True)
+
+    def test_load_given_the_non_existent_model_raises_FileNotFoundError(self):
+        self.assertRaises(
+            FileNotFoundError,
+            self._runner._load,
+            "this model has never, does not, and will never exist (hopefully)",
+        )
+
+    def test_run_produces_non_empty_file(self):
+        output = self._runner.run()
+        self.assertTrue(output.stat().st_size > 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
