3030d849d6129f74ea4e4922e1f64f679eeeda15
FrederikRothe <frederi.rothe@gmail.com>
2023-03-03 11:04:51 +0100

    added identical codeblocks test

---
+++ metrics/test/data/test_data_identical_codeblocks/data_identical_codeblocks.py
@@ -0,0 +1,39 @@
+def method1():
+    i = 0
+    if i == 0:
+        for z in range(10):
+            print(z)
+
+
+def method2():
+    x = 1
+    if x == 1:
+        for z in range(10):
+            print(z)
+
+def method3():
+    pass
+
+
+def method4():
+    i = 2
+    method1()
+    print(i)
+    print(i)
+    print(i)
+    print(i)
+    print(i)
+    print(i)
+    print(i)
+
+
+def method5():
+    i = 2
+    method1()
+    print(i)
+    print(i)
+    print(i)
+    print(i)
+    print(i)
+    print(i)
+    print(i)
---
+++ metrics/test/test_identical_codeblocks.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+import subprocess
+import unittest
+import os
+
+class TestFileLength(unittest.TestCase):
+    def setUp(self):
+        os.chdir("../../")
+
+    def test_identical_codeblocks_given_file_returns_1(self):
+        cmd = ["./metrics/identical_codeblocks.py", "./metrics/test/data/test_data_identical_codeblocks"]
+        process = subprocess.run(cmd, stdout=subprocess.PIPE)
+        result = process.stdout.decode("utf-8").strip()
+        self.assertEqual(result, "1")
+    
+    def tearDown(self):
+        os.chdir(Path("metrics/test").resolve())
+        
+if __name__ == "__main__":
+    unittest.main()
