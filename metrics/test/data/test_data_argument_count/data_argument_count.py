65ff5da653affee6b6974e0162a61343505f714f
FrederikRothe <frederi.rothe@gmail.com>
2023-03-03 09:51:40 +0100

    restructured

--- metrics/test/data/test_data_argument_count/data_argument_count.py
+++ metrics/test/data/test_data_argument_count/data_argument_count.py
@@ -15,3 +15,10 @@ def method3(arg1, arg2, arg3, arg4, arg5):
 ##This one violates.
 def method1(arg1, arg2, arg3, arg4, arg5, arg6):
     pass
+
+def do_something(arg1, arg2, arg3):
+    if(True):
+        i = 4
+        i += 1
+        if(i <= 5):
+            i += 1   
