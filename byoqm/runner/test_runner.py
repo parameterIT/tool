dfba5b0b37fc39cd0612c7cb1eb767279402ce8c
Christoffer Gram <chkg@itu.dk>
2023-03-03 10:27:53 +0100

    Add run test + setUp and tearDown of output folder for tests

--- byoqm/runner/test_runner.py
+++ byoqm/runner/test_runner.py
@@ -1,12 +1,29 @@
 import unittest
 
 from pathlib import Path
+
+from test.test_support import os, shutil
+
 from byoqm.runner import Runner
+
+_TEST_FOLDER = Path("byoqm/runner/test")
+_OUTPUT_FOLDER = _TEST_FOLDER / Path("test")
 
 
 class TestRunner(unittest.TestCase):
     def setUp(self):
-        self._runner = Runner("code_climate", Path("."))
+        _OUTPUT_FOLDER.mkdir(parents=True, exist_ok=True)
+
+        self._runner = Runner(
+            "code_climate",
+            Path("byoqm/"),
+            _OUTPUT_FOLDER,
+            True,
+        )
+
+    def tearDown(self):
+        # shutil over os.rmdir, because it allows you to remove non/empty directories
+        shutil.rmtree(_TEST_FOLDER, ignore_errors=True)
 
     def test_load_given_code_climate_inits_runner(self):
         self._runner._load("code_climate")
@@ -20,6 +37,10 @@ class TestRunner(unittest.TestCase):
             self._runner._load,
             "this model has never, does not, and will never exist (hopefully)",
         )
+
+    def test_run_produces_non_empty_file(self):
+        output = self._runner.run()
+        self.assertTrue(output.stat().st_size > 0)
 
 
 if __name__ == "__main__":
