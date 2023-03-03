f2a091a28fd6dbbb1159f2f90a748cfb557a16b9
FrederikRothe <frederi.rothe@gmail.com>
2023-03-03 11:12:02 +0100

    added complex logic code

--- metrics/test/test_complex_logic.py
+++ metrics/test/test_complex_logic.py
@@ -3,15 +3,15 @@ import subprocess
 import unittest
 import os
 
-class TestIdenticalCodeBlocks(unittest.TestCase):
+class TestComplexLogic(unittest.TestCase):
     def setUp(self):
         os.chdir("../../")
 
-    def test_identical_codeblocks_given_file_returns_1(self):
-        cmd = ["./metrics/identical_codeblocks.py", "./metrics/test/data/test_data_identical_codeblocks"]
+    def test_complex_logic_given_file_returns_4(self):
+        cmd = ["./metrics/complex_logic.py", "./metrics/test/data/test_data_complex_logic"]
         process = subprocess.run(cmd, stdout=subprocess.PIPE)
         result = process.stdout.decode("utf-8").strip()
-        self.assertEqual(result, "1")
+        self.assertEqual(result, "4")
     
     def tearDown(self):
         os.chdir(Path("metrics/test").resolve())
