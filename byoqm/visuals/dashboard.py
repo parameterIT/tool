1434cdaf982d1f4aa8bbb25ee1b663113611d14c
Caspar Marschall <60236750+SadCheeto@users.noreply.github.com>
2023-03-03 11:28:14 +0100

    changed import (#41)

--- byoqm/visuals/dashboard.py
+++ byoqm/visuals/dashboard.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 from datetime import datetime
 import os
 import pandas as pd
-
+from .line import get_line
 from bokeh.layouts import gridplot
 from bokeh.plotting import show
 
@@ -11,7 +11,7 @@ class Dashboard:
     def show_graphs(self):
         data = self.get_data()
         # consider changing to broader term such as 'figures' if we plan on expanding the list to include other charts
-        line_figures = [line.get_line(data, key) for key in data]
+        line_figures = [get_line(data, key) for key in data]
         gridplots = gridplot(
             [
                 [line_figures[i], line_figures[i + 1]]
