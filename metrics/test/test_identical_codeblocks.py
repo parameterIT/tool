da81a472dcb507f6336dd12cbd7fb1c3cb6dc038
FrederikRothe <frederi.rothe@gmail.com>
2023-03-03 11:19:47 +0100

    added skipIf, if cpd does not exist

--- metrics/test/test_identical_codeblocks.py
+++ metrics/test/test_identical_codeblocks.py
@@ -7,6 +7,10 @@ class TestIdenticalCodeBlocks(unittest.TestCase):
     def setUp(self):
         os.chdir("../../")
 
+    @unittest.skipIf(
+        not os.path.exists("../../metrics/cpd"),
+        "CPD doesn't exist in the environment",
+    )
     def test_identical_codeblocks_given_file_returns_1(self):
         cmd = ["./metrics/identical_codeblocks.py", "./metrics/test/data/test_data_identical_codeblocks"]
         process = subprocess.run(cmd, stdout=subprocess.PIPE)
