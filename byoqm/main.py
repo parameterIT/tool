a95666b97fb95b2555145d0744ec82c26bf5a2e0
SadCheeto <caspar.marschall@me.com>
2023-03-03 09:21:38 +0100

    Added simple cli to main

--- byoqm/main.py
+++ byoqm/main.py
@@ -3,6 +3,7 @@ import sys
 from byoqm.runner import Runner
 from byoqm.visuals.dashboard import Dashboard
 from visuals import line
+import click
 
 # Assumes the project is being run from the root of the repository
 MODELS_PATH = "models"
@@ -16,16 +17,23 @@ def parse_src_root() -> Path:
 
     return path_to_src
 
-
-if __name__ == "__main__":
+@click.command()
+@click.argument("src_root")
+@click.argument("quality_model")
+@click.option("--output", "-o", type=str, required=False, help="The path to the output directory", default="./output")
+@click.option("--save-file", type=bool, required=False, help="Boolean determining whether or not a file should be saved in the output directory", default=True)
+@click.option("--show-graphs", type=bool, required=False, help="Boolean determining whether or not the dashboard should be saved", default=True)
+def load(src_root: str, quality_model: str, output: str = "./output", save_file: bool = True, show_graphs: bool = True):
     if len(sys.argv) < 3:
         print(
             f"Make sure to provide the path to source code and a name of a quality model to use currently have {sys.argv}"
         )
         exit(1)
-    path = Path("byoqm")
-    runner: Runner = Runner("code_climate", path)
+    runner: Runner = Runner(quality_model, Path(src_root), Path(output), save_file)
     runner.run()
-
-    dashboard = Dashboard()
-    dashboard.show_graphs()
+    if (show_graphs):
+        dashboard = Dashboard()
+        dashboard.show_graphs()
+    
+if __name__ == "__main__":
+    load()
