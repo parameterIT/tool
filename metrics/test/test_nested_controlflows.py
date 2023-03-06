b96ad79a42776ae8524ccd482e95a217aa914b06
SadCheeto <caspar.marschall@me.com>
2023-03-06 08:52:50 +0100

    cahnged to expect failure

--- metrics/test/test_nested_controlflows.py
+++ metrics/test/test_nested_controlflows.py
@@ -7,17 +7,15 @@ import os
 class TestNestedControlFlows(unittest.TestCase):
     def setUp(self):
         os.chdir("../../")
-
-    def test_nested_controlflow_given_file_returns_2(self):
+    @unittest.expectedFailure
+    def test_nested_controlflow_given_test_directory_returns_9(self):
         cmd = [
             "./metrics/nested_controlflows.py",
             "./metrics/test/data/test_data_nested_controlflows",
         ]
         process = subprocess.run(cmd, stdout=subprocess.PIPE)
         result = process.stdout.decode("utf-8").strip()
-        self.assertEqual(result, "8")
-        # match statements do not work so therefore the test only works with 8
-        # self.assertEqual(result, "9")
+        self.assertEqual(result, "9")
 
     def tearDown(self):
         os.chdir(Path("metrics/test").resolve())
