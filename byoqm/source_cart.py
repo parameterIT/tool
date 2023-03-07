c83e7382b4ee37f279f8760a594b5ecd66f3b728
FrederikRothe <frederi.rothe@gmail.com>
2023-03-07 14:33:52 +0100

    format

--- byoqm/source_cart.py
+++ byoqm/source_cart.py
@@ -1,17 +1,22 @@
 from ast import List
 from tree_sitter import Language, Parser
 
-class SourceCart():
+
+class SourceCart:
     def __init__(self):
         self.ast = ast
         self.language = Language("./build/my-languages.so", "python")
         self.parser = Parser()
         self.plugins = List()
+
     def getAST(self):
         pass
+
     def getLanguage(self):
         pass
+
     def getParser(self):
         pass
+
     def getSourceRoot(self):
         pass
