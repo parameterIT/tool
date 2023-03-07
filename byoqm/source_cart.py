dc50cd7c462be98a0fab9ee43ba0ea9a3879e622
FrederikRothe <frederi.rothe@gmail.com>
2023-03-07 14:33:02 +0100

    merged main

---
+++ byoqm/source_cart.py
@@ -0,0 +1,17 @@
+from ast import List
+from tree_sitter import Language, Parser
+
+class SourceCart():
+    def __init__(self):
+        self.ast = ast
+        self.language = Language("./build/my-languages.so", "python")
+        self.parser = Parser()
+        self.plugins = List()
+    def getAST(self):
+        pass
+    def getLanguage(self):
+        pass
+    def getParser(self):
+        pass
+    def getSourceRoot(self):
+        pass
