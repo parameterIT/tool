a4f61d7ba8bebc83530b73be00048709f9395d9b
Jakob <73228675+Kobo-coder@users.noreply.github.com>
2023-03-08 15:29:09 +0100

    change method_count to adhere to metric
    add tester_model for isolation of method count

--- metrics/test/test_method_count.py
+++ metrics/test/test_method_count.py
@@ -1,21 +1,21 @@
 from pathlib import Path
-import subprocess
 import unittest
 import os
+from byoqm.source_coordinator.source_coordinator import SourceCoordinator
+
+from metrics.method_count import MethodCount
 
 
 class TestMethodCount(unittest.TestCase):
     def setUp(self):
-        os.chdir("../../")
+        os.chdir("../..")
+        self._coordinator = SourceCoordinator("./metrics/test/data/test_data_method_count","python")
+        self._methodcount = MethodCount()
+        self._methodcount.set_coordinator(self._coordinator)
 
     def test_method_count_given_file_returns_1(self):
-        cmd = [
-            "./metrics/argument_count.py",
-            "./metrics/test/data/test_data_argument_count",
-        ]
-        process = subprocess.run(cmd, stdout=subprocess.PIPE)
-        result = process.stdout.decode("utf-8").strip()
-        self.assertEqual(result, "2")
+        result = self._methodcount.run()
+        self.assertEqual(result, "1")
 
     def tearDown(self):
         os.chdir(Path("metrics/test").resolve())
---
+++ models/tester_model.py
@@ -0,0 +1,30 @@
+from typing import Dict
+
+from tree_sitter import Language, Parser
+
+from byoqm.qualitymodel.qualitymodel import QualityModel
+
+
+class TesterModel(QualityModel):
+    def __init__(self):
+        self._py_language = Language("build/my-languages.so", "python")
+        self._parser = Parser()
+        self._parser.set_language(self._py_language)
+
+    def getDesc(self) -> Dict:
+        model = {
+            "metrics": {
+                "method_count": "./metrics/method_count.py",
+            },
+            "aggregations": {
+                "maintainability": self.maintainability,
+            },
+        }
+        return model
+
+    def maintainability(self, results: Dict) -> int | float:
+        return (
+            results["method_count"]
+        )
+
+model = TesterModel()
