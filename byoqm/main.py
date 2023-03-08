2012f8b6de7d1ca71c1e7f47b6e38e37b4733031
Jakob <73228675+Kobo-coder@users.noreply.github.com>
2023-03-08 17:21:01 +0100

    remove old sys arg check

--- byoqm/main.py
+++ byoqm/main.py
@@ -5,18 +5,6 @@ from byoqm.visuals.dashboard import Dashboard
 import click
 
 
-def parse_src_root() -> Path:
-    if len(sys.argv) == 1:
-        print("Make sure to provide the path to source code")
-        exit(1)
-    path_to_src = Path(sys.argv[1])
-    if not path_to_src.exists():
-        print(f"The source code at {path_to_src.resolve()} does not exist")
-        exit(1)
-
-    return path_to_src
-
-
 @click.command()
 @click.argument("src_root", required=True)
 @click.argument("quality_model", required=True)
