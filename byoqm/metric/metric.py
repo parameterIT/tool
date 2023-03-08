be3d47063c912171e5c46acbbd73915ace5d59f7
Jakob <73228675+Kobo-coder@users.noreply.github.com>
2023-03-08 15:26:22 +0100

    change metric to abstract class

--- byoqm/metric/metric.py
+++ byoqm/metric/metric.py
@@ -1,14 +1,10 @@
 from abc import ABC, abstractmethod
-import subprocess
 from pathlib import Path
 
-
-class Metric:
-    def __init__(self, path: Path):
-        self.path = path.resolve()
-        self.name = path.name
-
-    def measure(self) -> int | float:
-        process = subprocess.run([self.path], stdout=subprocess.PIPE)
-        result = process.stdout.decode("utf-8").strip()
-        return result
+class Metric(ABC):
+    def __init__(self):
+        pass
+    
+    @abstractmethod
+    def run(self) -> int | float:
+        pass
