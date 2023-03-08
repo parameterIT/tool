b2aca4865e691f5d054d0f552d2f86aaed04cedc
Jakob <73228675+Kobo-coder@users.noreply.github.com>
2023-03-08 10:31:06 +0100

    test

---
+++ metrics/test/data/test_data_similar_codeblocks/data_similar_codeblocks.py
@@ -0,0 +1,20 @@
+def method4():
+    i = 2
+    print(i)
+    print(i)
+    print(i)
+    print(i)
+    print(i)
+    print(i)
+    print(i)
+
+
+def method5():
+    i = 2
+    print(i)
+    print(i)
+    print(i)
+    print(i)
+    print(i)
+    print(i)
+    print(i)
---
+++ metrics/test/test_similar_codeblocks.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+import subprocess
+import unittest
+import os
+
+
+class TestIdenticalCodeBlocks(unittest.TestCase):
+    def setUp(self):
+        os.chdir("../../")
+
+    @unittest.skipIf(
+        not os.path.exists("../../metrics/cpd"),
+        "CPD doesn't exist in the environment",
+    )
+    def test_identical_codeblocks_given_file_returns_1(self):
+        cmd = [
+            "./metrics/similar_codeblocks.py",
+            "./metrics/test/data/test_data_similar_codeblocks",
+        ]
+        process = subprocess.run(cmd, stdout=subprocess.PIPE)
+        result = process.stdout.decode("utf-8").strip()
+        self.assertEqual(result, "1")
+
+    def tearDown(self):
+        os.chdir(Path("metrics/test").resolve())
+
+
+if __name__ == "__main__":
+    unittest.main()
