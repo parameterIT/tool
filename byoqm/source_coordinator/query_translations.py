b3ace622c5f53667e77e366ca70c5b3f205fa1dd
Jakob <73228675+Kobo-coder@users.noreply.github.com>
2023-03-10 15:38:35 +0100

    add query translations

--- byoqm/source_coordinator/query_translations.py
+++ byoqm/source_coordinator/query_translations.py
@@ -1,10 +1,30 @@
 query_lang = {
 
     "python" : {
-        "function" : ""
+        "parameters" : "parameters: (parameters)",
+        "bool_operator" : "condition: (boolean_operator) right: (boolean_operator)",
+        "comment" : "(comment)",
+        "function" : "(function_definition)",
+        "function_block" : "(function_definition body: (block) @function.block)",
+        "return" : "(return_statement)",
    },
 
    "c_sharp" : {
-        "function" : "(method_declaration) (class_declaration)"
+        "parameters" : "parameters: (parameter_list)",
+        "bool_operator" : "condition: (binary_expression) (binary_expression)",
+        "comment" : "(comment)",
+        "function" : "(method_declaration) (constructor_declaration)",
+        "function_block" : "(method_declaration body: (block) @function.block) (constructor_declaration body: (block) @function.block)",
+        "return" : "(return_statement)"
+   },
+
+   "java" : {
+        "parameters" : "parameters: (formal_parameters)",
+        "bool_operator" : "(binary_expression)",
+        "comment" : "(line_comment) (block_comment)",
+        "function" : "(method_declaration) (constructor_declaration)",
+        "function_block" : "(method_declaration body: (block) @function.block) (constructor_declaration body: (constructor_body) @function.block)",
+        "return" : "(return_statement)"
    }
+
 }
