71f68b524421da9c8418a843e040544b3b8b9266
FrederikRothe <frederi.rothe@gmail.com>
2023-03-10 12:27:27 +0100

    ignoring comments

--- metrics/test/data/test_data_file_length/data_file_length.py
+++ metrics/test/data/test_data_file_length/data_file_length.py
@@ -1,5 +1,7 @@
 # This is a very long file to trigger the > 250 condition in CodeClimate. It is non-sensical
+# Total of 251 lines, excluding blank lines and comments
 for i in range(10):
+    print(i)
     print(i)
     print(i)
     print(i)
