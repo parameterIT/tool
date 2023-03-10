1171e0d01aff9627bda590ded7e6600bf42fdd24
Christoffer Gram <chkg@itu.dk>
2023-03-10 14:07:36 +0100

    Update Names

--- models/code_climate.py
+++ models/code_climate.py
@@ -9,20 +9,20 @@ class CodeClimate(QualityModel):
     def getDesc(self) -> Dict:
         model = {
             "metrics": {
-                "method_length": "./metrics/method_length.py",
-                "file_length": "./metrics/file_length.py",
+                "method_lines": "./metrics/method_length.py",
+                "file_lines": "./metrics/file_length.py",
                 "argument_count": "./metrics/argument_count.py",
                 "complex_logic": "./metrics/complex_logic.py",
                 "method_count": "./metrics/method_count.py",
                 "return_statements": "./metrics/return_statements.py",
-                "identical_blocks_of_code": "./metrics/identical_codeblocks.py",
-                "similar_blocks_of_code": "./metrics/similar_codeblocks.py",
-                "nested_controlflows": "./metrics/nested_controlflows.py",
+                "identical-code": "./metrics/identical_codeblocks.py",
+                "similar-code": "./metrics/similar_codeblocks.py",
+                "nested_control_flow": "./metrics/nested_controlflows.py",
             },
             "aggregations": {
-                "cognitive_complexity": self.cognitive_complexity,
-                "complexity": self.complexity,
-                "duplication": self.duplication,
+                "method_complexity": self.cognitive_complexity,
+                "Complexity": self.complexity,
+                "Duplication": self.duplication,
             },
         }
         return model
