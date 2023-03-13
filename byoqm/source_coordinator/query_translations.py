de3e9c68f03af5f3113961b3d21b7bc923ee1f0b
SadCheeto <caspar.marschall@me.com>
2023-03-13 12:13:08 +0100

    complex logic tests

--- byoqm/source_coordinator/query_translations.py
+++ byoqm/source_coordinator/query_translations.py
@@ -2,6 +2,7 @@ query_lang = {
     "python": {
         "parameters": "parameters: (parameters)",
         "bool_operator": "condition: (boolean_operator) right: (boolean_operator)",
+        "bool_operator_child": "boolean_operator",
         "comment": "(comment)",
         "function": "(function_definition)",
         "function_block": "(function_definition body: (block) @function.block)",
@@ -49,6 +50,7 @@ query_lang = {
     "c_sharp": {
         "parameters": "parameters: (parameter_list)",
         "bool_operator": "condition: (binary_expression) (binary_expression)",
+        "bool_operator_child": "binary_expression",
         "comment": "(comment)",
         "function": "(method_declaration) (constructor_declaration)",
         "function_block": "(method_declaration body: (block) @function.block) (constructor_declaration body: (block) @function.block)",
@@ -100,6 +102,7 @@ query_lang = {
     "java": {
         "parameters": "parameters: (formal_parameters)",
         "bool_operator": "(binary_expression)",
+        "bool_operator_child": "binary_expression",
         "comment": "(line_comment) (block_comment)",
         "function": "(method_declaration) (constructor_declaration)",
         "function_block": "(method_declaration body: (block) @function.block) (constructor_declaration body: (constructor_body) @function.block)",
--- metrics/complex_logic.py
+++ metrics/complex_logic.py
@@ -29,10 +29,25 @@ class ComplexLogic(Metric):
             # initial count is always at least 2 (right and left)
             boolean_count = 2
             node = capture[0]
-            while node.child_by_field_name("left").type == "boolean_operator":
-                boolean_count += 1
-                node = node.child_by_field_name("left")
-                # change the value below to a parameter when parameterizing
+            bool_operator = query_lang[self.coordinator.prog_lang][
+                "bool_operator_child"
+            ]
+            children = [
+                node.child_by_field_name("left"),
+                node.child_by_field_name("right"),
+            ]
+            while len(children) > 0:
+                if boolean_count > 4:
+                    break
+                child = children.pop()
+                if child.type == bool_operator:
+                    boolean_count += 1
+                    children.extend(
+                        [
+                            child.child_by_field_name("left"),
+                            child.child_by_field_name("right"),
+                        ]
+                    )
             if boolean_count > 4:
                 count += 1
         return count
