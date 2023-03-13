35468bf8aa23d702494a1906481cb877877fa751
SadCheeto <caspar.marschall@me.com>
2023-03-13 08:51:24 +0100

    Method count c sharp test

---
+++ metrics/test/data/test_data_method_count/data_method_count_1.cs
@@ -0,0 +1,98 @@
+public class TestClass 
+{
+    public TestClass() { }
+
+    public void method1() 
+    {
+        Console.WriteLine("hello world");
+    }
+    public void method2() 
+    {
+        Console.WriteLine("hello world");
+    }
+    public void method3() 
+    {
+        Console.WriteLine("hello world");
+    }
+    public void method4() 
+    {
+        Console.WriteLine("hello world");
+    }
+    public void method5() 
+    {
+        Console.WriteLine("hello world");
+    }
+    public void method6() 
+    {
+        Console.WriteLine("hello world");
+    }
+    public void method7() 
+    {
+        Console.WriteLine("hello world");
+    }
+    public void method8() 
+    {
+        Console.WriteLine("hello world");
+    }
+    public void method9() 
+    {
+        Console.WriteLine("hello world");
+    }
+    public void method10() 
+    {
+        Console.WriteLine("hello world");
+    }
+    public void method11() 
+    {
+        Console.WriteLine("hello world");
+    }
+    public void method12() 
+    {
+        Console.WriteLine("hello world");
+    }
+    public void method13() 
+    {
+        Console.WriteLine("hello world");
+    }
+    public void method14() 
+    {
+        Console.WriteLine("hello world");
+    }
+    public void method15() 
+    {
+        Console.WriteLine("hello world");
+    }
+    public void method16() 
+    {
+        Console.WriteLine("hello world");
+    }
+    public void method16() 
+    {
+        Console.WriteLine("hello world");
+    }
+    public void method17() 
+    {
+        Console.WriteLine("hello world");
+    }
+    public void method18() 
+    {
+        Console.WriteLine("hello world");
+    }
+    public void method19() 
+    {
+        Console.WriteLine("hello world");
+    }
+    public void method20() 
+    {
+        Console.WriteLine("hello world");
+    }
+    public void method21() 
+    {
+        Console.WriteLine("hello world");
+    }
+    public void method22() 
+    {
+        Console.WriteLine("hello world");
+    }
+
+}
