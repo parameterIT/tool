6215c4e582a6496cd33f0736a175298256525a40
Jakob <73228675+Kobo-coder@users.noreply.github.com>
2023-03-13 08:58:53 +0100

    add tests for file length

---
+++ metrics/test/data/test_data_file_length/data_file_length.cs
@@ -0,0 +1,273 @@
+public class TestClass 
+{
+    public TestClass() { }
+
+    public Method() {
+        //This shouldn't count
+        //This shouldn't count
+        //This shouldn't count
+        //This shouldn't count
+        //This shouldn't count
+        //This shouldn't count
+        //This shouldn't count
+        //This shouldn't count
+        //This shouldn't count
+        //This shouldn't count
+        //This shouldn't count
+        //This shouldn't count
+        //This shouldn't count
+        //This shouldn't count
+        //This shouldn't count
+        //This shouldn't count
+        //This shouldn't count
+        //This shouldn't count
+        //This shouldn't count
+        //This shouldn't count
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+    }
+}
