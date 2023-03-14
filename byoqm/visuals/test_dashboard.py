6952013674a7eb727cc6b50253444a3489ad1372
FrederikRothe <frederi.rothe@gmail.com>
2023-03-14 13:43:30 +0100

    format

--- byoqm/visuals/test_dashboard.py
+++ byoqm/visuals/test_dashboard.py
@@ -42,21 +42,27 @@ class TestDashboard(unittest.TestCase):
         start_date = datetime.min
         end_date = datetime.max
         dashboard = Dashboard(start_date, end_date)
-        data = dashboard.get_data(path=_OUTPUT_FOLDER, in_use_qm="some_model", targetPath="dummy_test")
+        data = dashboard.get_data(
+            path=_OUTPUT_FOLDER, in_use_qm="some_model", targetPath="dummy_test"
+        )
         self.assertEqual(len(data[_METRIC_NAME]), 5)
 
     def test_get_data_given_2023_returns_4(self):
         start_date = datetime.strptime("2023-01-01", "%Y-%m-%d")
         end_date = datetime.strptime("2023-12-31", "%Y-%m-%d")
         dashboard = Dashboard(start_date, end_date)
-        data = dashboard.get_data(path=_OUTPUT_FOLDER, in_use_qm="some_model", targetPath="dummy_test")
+        data = dashboard.get_data(
+            path=_OUTPUT_FOLDER, in_use_qm="some_model", targetPath="dummy_test"
+        )
         self.assertEqual(len(data[_METRIC_NAME]), 4)
 
     def test_get_data_given_1_day_seperation_returns_2(self):
         start_date = datetime.strptime("2023-03-02", "%Y-%m-%d")
         end_date = datetime.strptime("2023-03-03", "%Y-%m-%d")
         dashboard = Dashboard(start_date, end_date)
-        data = dashboard.get_data(path=_OUTPUT_FOLDER, in_use_qm="some_model", targetPath="dummy_test")
+        data = dashboard.get_data(
+            path=_OUTPUT_FOLDER, in_use_qm="some_model", targetPath="dummy_test"
+        )
         self.assertEqual(len(data[_METRIC_NAME]), 2)
 
 
