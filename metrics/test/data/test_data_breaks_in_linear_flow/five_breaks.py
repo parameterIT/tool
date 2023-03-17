843d959a4fee37ac54706b4935280fb234ac0415
Christoffer Gram <chkg@itu.dk>
2023-03-17 14:25:03 +0100

    Add Breaks in Linear Flow

---
+++ metrics/test/data/test_data_breaks_in_linear_flow/five_breaks.py
@@ -0,0 +1,24 @@
+for i in range(10):
+    print(i)
+
+while True:
+    print("looping...")
+
+if False:
+    print("True")
+    if "nested" == "if":
+        pass
+    elif "nested" == "elif":
+        pass
+    else "nested" == "else":
+        pass
+elif True:
+    print("False")
+else:
+    print("Neither true nor false")
+
+try:
+    print("will fail")
+    raise ValueError
+except:
+    print("failed")
