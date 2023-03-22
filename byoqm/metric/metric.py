37501ef9792accf4287304e6cfd0ec22b7326523
Jakob <73228675+Kobo-coder@users.noreply.github.com>
2023-03-22 13:55:46 +0100

    change interface

--- byoqm/metric/metric.py
+++ byoqm/metric/metric.py
@@ -1,13 +1,15 @@
 from abc import ABC, abstractmethod
 from pathlib import Path
 
+from byoqm.metric.result import Result
+
 
 class Metric(ABC):
     def __init__(self):
         pass
 
     @abstractmethod
-    def run(self) -> int | float:
+    def run(self) -> Result:
         """
         run returns a number that is the measurement of that specific metric.
         """
