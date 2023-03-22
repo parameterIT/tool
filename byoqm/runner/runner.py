6b01db3ae58db51ce7f410b615cbc0e8db466062
Jakob <73228675+Kobo-coder@users.noreply.github.com>
2023-03-22 13:56:58 +0100

    make runner work with new interface

--- byoqm/runner/runner.py
+++ byoqm/runner/runner.py
@@ -13,6 +13,7 @@ from typing import Dict, List
 
 from test.test_support import os
 from byoqm.metric.metric import Metric
+from byoqm.metric.result import Result
 
 from byoqm.qualitymodel.qualitymodel import QualityModel
 from byoqm.source_repository.source_repository import SourceRepository
@@ -101,13 +102,15 @@ class Runner:
 
     def _generate_violations_table(self, results: Dict, time: str):
         list_of_violations = []
-        for _, violations in results.items():
-            if type(violations) is list:
-                list_of_violations.extend(violations)
+        for _, result in results.items():
+            # need a better name than result...
+            if type(result) is not Result:
+                continue
+            for location in result.get_violation_locations():
+                list_of_violations.extend([[result.metric, location]])
         violations = pd.DataFrame(
-            list_of_violations, columns=["type", "file", "start", "end"]
+            list_of_violations, columns=["type", "file_start_end"]
         )
-        violations.attrs = {"model": self._model_name, "root": self._src_root}
         file_location = Path(
             self._output_dir / Path("violations") / Path(time + ".csv")
         )
@@ -131,8 +134,8 @@ class Runner:
             writer.writerow(["metric", "value"])
             for description, value in results.items():
                 frequency = value
-                if type(value) is list:
-                    frequency = len(value)
+                if type(value) is Result:
+                    frequency = value.get_frequency()
                 writer.writerow([description, frequency])
         logging.info("Finished writing to csv")
         return file_location
