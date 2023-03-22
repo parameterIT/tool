2f654de9ffa0ac5aafe13a019f6b5bf498202144
FrederikRothe <frederi.rothe@gmail.com>
2023-03-22 13:55:23 +0100

    so far so good, working on date format

--- figures/line.py
+++ figures/line.py
@@ -1,6 +1,8 @@
+from cmath import pi
 from collections import defaultdict
 from bokeh.plotting import figure
 from byoqm.dashboard.figure import Figure
+from bokeh.models import DatetimeTickFormatter
 
 
 class LineChart(Figure):
