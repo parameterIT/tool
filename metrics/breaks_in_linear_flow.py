285180b21fed10791030827ed4bfb70ec191aeb2
Jakob <73228675+Kobo-coder@users.noreply.github.com>
2023-03-22 15:05:08 +0100

    change newly added metrics to adhere to result/violation interface

--- metrics/breaks_in_linear_flow.py
+++ metrics/breaks_in_linear_flow.py
@@ -4,6 +4,8 @@ from pathlib import Path
 from typing import List
 
 from byoqm.metric.metric import Metric
+from byoqm.metric.result import Result
+from byoqm.metric.violation import Violation
 from byoqm.source_repository.query_translations import translate_to
 from byoqm.source_repository.source_repository import SourceRepository
 
@@ -17,12 +19,12 @@ class BreaksInLinearFlow(Metric):
         Counts the number of occurences of looping strucctures and conditional
         structures
         """
-        results = []
+        result = Result("breaks in linear flow",[])
         for file_path in self._source_repository.src_paths:
-            results.extend(self._count_control_flow_statement(file_path))
-        return results
+            self._count_control_flow_statement(file_path,result)
+        return result
 
-    def _count_control_flow_statement(self, file_path: Path):
+    def _count_control_flow_statement(self, file_path: Path, result: Result):
         query = self._source_repository.tree_sitter_language.query(
             f"""
             [
@@ -38,6 +40,17 @@ class BreaksInLinearFlow(Metric):
 
         ast: tree_sitter.Tree = self._source_repository.getAst(file_path)
         captures = query.captures(ast.root_node)
+        for node,_ in captures:
+            result.append(
+                Violation(
+                    "breaks in linear flow",
+                    (
+                     str(file_path),
+                     node.start_point[0]+1,
+                     node.end_point[0]
+                    )
+                )
+            )
         results = [("Breaks In Linear Flow", file_path, 1, 1) for _, _ in captures]
         return results
 
