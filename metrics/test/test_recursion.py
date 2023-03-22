00ba7359da2d8ddda50f6caa4c6cbf31abb2d729
SadCheeto <caspar.marschall@me.com>
2023-03-22 13:55:47 +0100

    black

--- metrics/test/test_recursion.py
+++ metrics/test/test_recursion.py
@@ -25,6 +25,7 @@ class TestRecursion(unittest.TestCase):
         actual = len(self._metric.run())
         expected = 1
         self.assertEqual(actual, expected)
+
     def test_recursion_given_c_sharp_file_returns_1(self):
         new_source_repository = SourceRepository(
             Path("./metrics/test/data/test_data_recursion"), "c_sharp"
@@ -34,7 +35,7 @@ class TestRecursion(unittest.TestCase):
         recursion._source_repository = new_source_repository
         result = len(recursion.run())
         self.assertEqual(result, 1)
-        
+
     def test_recursion_given_java_file_returns_1(self):
         new_source_repository = SourceRepository(
             Path("./metrics/test/data/test_data_recursion"), "java"
