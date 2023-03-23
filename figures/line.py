d204fbe024850ccf032401c47eb7edf8b9725e54
FrederikRothe <frederi.rothe@gmail.com>
2023-03-23 10:57:35 +0100

    dates now work a bit better

--- figures/line.py
+++ figures/line.py
@@ -9,7 +9,6 @@ class LineChart(Figure):
     def __init__(self):
         self._data: dict = defaultdict(list)
 
-    # this needs to get changed for monthly shift
     def _get_line(self, data, key):
         x = [(value[0]) for value in data[key]]
         y = [(value[1]) for value in data[key]]
