5fe2aaeef170839a75f0d7a6b56d2aa6d0f14b32
Jakob <73228675+Kobo-coder@users.noreply.github.com>
2023-03-24 10:48:04 +0100

    fix nested control flow test

--- metrics/test/test_nested_controlflows.py
+++ metrics/test/test_nested_controlflows.py
@@ -23,17 +23,19 @@ class TestNestedControlFlows(unittest.TestCase):
 
     def test_nested_controlflow_given_test_directory_returns_9(self):
         result = self._nestedflow.run()
-        locations = result.get_violation_locations()
         self.assertEqual(result.get_frequency(), 9)
-        self.assertEqual((locations[0][1], locations[0][2]), (3, 10))
-        self.assertEqual((locations[1][1], locations[1][2]), (4, 8))
-        self.assertEqual((locations[2][1], locations[2][2]), (10, 14))
-        self.assertEqual((locations[3][1], locations[3][2]), (1, 6))
-        self.assertEqual((locations[4][1], locations[4][2]), (8, 13))
-        self.assertEqual((locations[5][1], locations[5][2]), (3, 19))
-        self.assertEqual((locations[6][1], locations[6][2]), (20, 32))
-        self.assertEqual((locations[7][1], locations[7][2]), (45, 49))
-        self.assertEqual((locations[8][1], locations[8][2]), (51, 59))
+        reported_lines = set()
+        for location in result.get_violation_locations():
+            reported_lines.add((location[1],location[2]))
+        self.assertIn((3, 10),reported_lines)
+        self.assertIn((4, 8),reported_lines)
+        self.assertIn((10, 14),reported_lines)
+        self.assertIn((1, 6),reported_lines)
+        self.assertIn((8, 13),reported_lines)
+        self.assertIn((3, 19),reported_lines)
+        self.assertIn((20, 32),reported_lines)
+        self.assertIn((45, 49),reported_lines)
+        self.assertIn((51, 59),reported_lines)
 
     def test_nested_control_flow_given_c_sharp_file_returns_6(self):
         new_source_repository = SourceRepository(
