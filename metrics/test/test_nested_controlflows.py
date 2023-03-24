667eba62f00d910d4b63356a121b0e70e049a424
Jakob <73228675+Kobo-coder@users.noreply.github.com>
2023-03-24 10:51:29 +0100

    format

--- metrics/test/test_nested_controlflows.py
+++ metrics/test/test_nested_controlflows.py
@@ -26,16 +26,16 @@ class TestNestedControlFlows(unittest.TestCase):
         self.assertEqual(result.get_frequency(), 9)
         reported_lines = set()
         for location in result.get_violation_locations():
-            reported_lines.add((location[1],location[2]))
-        self.assertIn((3, 10),reported_lines)
-        self.assertIn((4, 8),reported_lines)
-        self.assertIn((10, 14),reported_lines)
-        self.assertIn((1, 6),reported_lines)
-        self.assertIn((8, 13),reported_lines)
-        self.assertIn((3, 19),reported_lines)
-        self.assertIn((20, 32),reported_lines)
-        self.assertIn((45, 49),reported_lines)
-        self.assertIn((51, 59),reported_lines)
+            reported_lines.add((location[1], location[2]))
+        self.assertIn((3, 10), reported_lines)
+        self.assertIn((4, 8), reported_lines)
+        self.assertIn((10, 14), reported_lines)
+        self.assertIn((1, 6), reported_lines)
+        self.assertIn((8, 13), reported_lines)
+        self.assertIn((3, 19), reported_lines)
+        self.assertIn((20, 32), reported_lines)
+        self.assertIn((45, 49), reported_lines)
+        self.assertIn((51, 59), reported_lines)
 
     def test_nested_control_flow_given_c_sharp_file_returns_6(self):
         new_source_repository = SourceRepository(
