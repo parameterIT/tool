af58d3d0586a54490a4e534c49d584b246a955a1
SadCheeto <caspar.marschall@me.com>
2023-04-11 09:49:54 +0200

    changed similar code to use new encoding system

--- metrics/similar_codeblocks.py
+++ metrics/similar_codeblocks.py
@@ -24,27 +24,33 @@ class SimilarBlocksofCode(Metric):
         Finds the amount of similar code blocks in a given repository
         """
         result = Result("similar code", [])
-        filestring = f"{files}"
-        filestring = filestring[1 : len(filestring) - 1]
-        res = subprocess.run(
-            f"metrics/cpd/bin/run.sh cpd --minimum-tokens {TOKENS} --skip-lexical-errors --ignore-identifiers --ignore-literals --dir {filestring} --format xml",
-            shell=True,
-            capture_output=True,
-            text=True,
-        )
-        element_tree = parse(StringIO(res.stdout))
-        for child in element_tree.getroot():
-            if child.tag == "duplication":
-                duplicates = [
-                    (
-                        child.attrib["path"],
-                        int(child.attrib["line"]),
-                        int(child.attrib["endline"]),
-                    )
-                    for child in child
-                    if child.tag == "file"
-                ]
-                result.violations.append(Violation("similar code", duplicates))
+        for encoding in set(self._source_repository.file_encodings.values()):
+            files = [
+                str(file)
+                for file, encoding_type in self._source_repository.file_encodings.items()
+                if encoding_type == encoding
+            ]
+            filestring = f"{files}"
+            filestring = filestring[1 : len(filestring) - 1]
+            res = subprocess.run(
+                f"metrics/cpd/bin/run.sh cpd --minimum-tokens {TOKENS} --skip-lexical-errors --dir {filestring} --format xml --encoding {encoding}",
+                shell=True,
+                capture_output=True,
+                text=True,
+            )
+            element_tree = parse(StringIO(res.stdout))
+            for child in element_tree.getroot():
+                if child.tag == "duplication":
+                    duplicates = [
+                        (
+                            child.attrib["path"],
+                            int(child.attrib["line"]),
+                            int(child.attrib["endline"]),
+                        )
+                        for child in child
+                        if child.tag == "file"
+                    ]
+                    result.violations.append(Violation("similar code", duplicates))
         return result
 
 
