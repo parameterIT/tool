2c2bccc3af8d2696e0c04cc60666da794821f046
FrederikRothe <frederi.rothe@gmail.com>
2023-04-17 11:52:37 +0200

    format

--- metrics/identical_codeblocks.py
+++ metrics/identical_codeblocks.py
@@ -38,7 +38,7 @@ class IdenticalBlocksofCode(Metric):
             cpd_encoding = _CHARDET_ENCODINGS_TO_CPD[file_info.encoding]
 
             res = subprocess.run(
-                f"metrics/cpd/bin/run.sh cpd --minimum-tokens {TOKENS} --skip-lexical-errors --dir \"{filestring}\" --format xml --encoding {cpd_encoding}",
+                f'metrics/cpd/bin/run.sh cpd --minimum-tokens {TOKENS} --skip-lexical-errors --dir "{filestring}" --format xml --encoding {cpd_encoding}',
                 shell=True,
                 capture_output=True,
                 text=True,
