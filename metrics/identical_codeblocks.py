5ce4873b196c4b25a5be75dc4e87a6537e7099da
Jakob <73228675+Kobo-coder@users.noreply.github.com>
2023-04-26 15:05:02 +0200

    add fix to cpd (#111)
    * add fix to cpd
    
    * fix similar code
    
    * remove encoding check from similar code

--- metrics/identical_codeblocks.py
+++ metrics/identical_codeblocks.py
@@ -8,16 +8,6 @@ import subprocess
 
 TOKENS = 35
 
-_CHARDET_ENCODINGS_TO_CPD = {
-    "ASCII": "US-ASCII",
-    "ISO-8859-1": "ISO-8859-1",
-    "UTF-8": "UTF-8",
-    "UTF-8-SIG": "UTF-8",
-    "UTF-16": "UTF-16",
-    "UTF-16BE": "UTF-16BE",
-    "UTF-16LE": "UTF-16LE",
-}
-
 
 class IdenticalBlocksofCode(Metric):
     def __init__(self):
@@ -33,29 +23,28 @@ class IdenticalBlocksofCode(Metric):
         Makes use of Copy Paste Detector (CPD)
         """
         violations = []
-        for file, file_info in self._source_repository.files.items():
-            filestring = f"{file}"
-            cpd_encoding = _CHARDET_ENCODINGS_TO_CPD[file_info.encoding]
-
-            res = subprocess.run(
-                f'metrics/cpd/bin/run.sh cpd --minimum-tokens {TOKENS} --skip-lexical-errors --dir "{filestring}" --format xml --encoding {cpd_encoding}',
-                shell=True,
-                capture_output=True,
-                text=True,
-            )
-            element_tree = parse(StringIO(res.stdout))
-            for child in element_tree.getroot():
-                if child.tag == "duplication":
-                    duplicates = [
-                        (
-                            child.attrib["path"],
-                            int(child.attrib["line"]),
-                            int(child.attrib["endline"]),
-                        )
-                        for child in child
-                        if child.tag == "file"
-                    ]
-                    violations.append(Violation("identical code", duplicates))
+        to_inspect = [str(file) for file, _ in self._source_repository.files.items()]
+        to_inspect = str(to_inspect)
+        to_inspect = to_inspect[1 : (len(to_inspect) - 1)]
+        res = subprocess.run(
+            f"metrics/cpd/bin/run.sh cpd --minimum-tokens {TOKENS} --skip-lexical-errors --dir {to_inspect} --format xml",
+            shell=True,
+            capture_output=True,
+            text=True,
+        )
+        element_tree = parse(StringIO(res.stdout))
+        for child in element_tree.getroot():
+            if child.tag == "duplication":
+                duplicates = [
+                    (
+                        child.attrib["path"],
+                        int(child.attrib["line"]),
+                        int(child.attrib["endline"]),
+                    )
+                    for child in child
+                    if child.tag == "file"
+                ]
+                violations.append(Violation("identical code", duplicates))
         return Result("identical code", violations, len(violations))
 
 
--- metrics/similar_codeblocks.py
+++ metrics/similar_codeblocks.py
@@ -9,16 +9,6 @@ from defusedxml.ElementTree import parse
 
 TOKENS = 35
 
-_CHARDET_ENCODINGS_TO_CPD = {
-    "ASCII": "US-ASCII",
-    "ISO-8859-1": "ISO-8859-1",
-    "UTF-8": "UTF-8",
-    "UTF-8-SIG": "UTF-8",
-    "UTF-16": "UTF-16",
-    "UTF-16BE": "UTF-16BE",
-    "UTF-16LE": "UTF-16LE",
-}
-
 
 class SimilarBlocksofCode(Metric):
     def __init__(self):
@@ -32,29 +22,28 @@ class SimilarBlocksofCode(Metric):
         Finds the amount of similar code blocks in a given repository
         """
         violations = []
-        for file, file_info in self._source_repository.files.items():
-            filestring = f"{file}"
-            cpd_encoding = _CHARDET_ENCODINGS_TO_CPD[file_info.encoding]
-
-            res = subprocess.run(
-                f'metrics/cpd/bin/run.sh cpd --minimum-tokens {TOKENS} --skip-lexical-errors --ignore-identifiers --ignore-literals --dir "{filestring}" --format xml --encoding {cpd_encoding}',
-                shell=True,
-                capture_output=True,
-                text=True,
-            )
-            element_tree = parse(StringIO(res.stdout))
-            for child in element_tree.getroot():
-                if child.tag == "duplication":
-                    duplicates = [
-                        (
-                            child.attrib["path"],
-                            int(child.attrib["line"]),
-                            int(child.attrib["endline"]),
-                        )
-                        for child in child
-                        if child.tag == "file"
-                    ]
-                    violations.append(Violation("similar code", duplicates))
+        to_inspect = [str(file) for file, _ in self._source_repository.files.items()]
+        to_inspect = str(to_inspect)
+        to_inspect = to_inspect[1 : (len(to_inspect) - 1)]
+        res = subprocess.run(
+            f"metrics/cpd/bin/run.sh cpd --minimum-tokens {TOKENS} --skip-lexical-errors --ignore-identifiers --ignore-literals --dir {to_inspect} --format xml",
+            shell=True,
+            capture_output=True,
+            text=True,
+        )
+        element_tree = parse(StringIO(res.stdout))
+        for child in element_tree.getroot():
+            if child.tag == "duplication":
+                duplicates = [
+                    (
+                        child.attrib["path"],
+                        int(child.attrib["line"]),
+                        int(child.attrib["endline"]),
+                    )
+                    for child in child
+                    if child.tag == "file"
+                ]
+                violations.append(Violation("similar code", duplicates))
         return Result("similar code", violations, len(violations))
 
 
